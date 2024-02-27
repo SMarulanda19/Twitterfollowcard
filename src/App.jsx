5d975f09e52515c35a5c279af3fc0d45f7802f93
SMarulanda19 <s.quintero@globant.com>
2024-02-26 21:50:53 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { TwitterFollowCard } from './TwitterFollowCard'
-import './app.css'
+import { TwitterFollowCard } from './TwitterFollowCard';
+import './app.css';
 
-export function App () {
-const format = (userName) => '@${userName}'
-    return(
-    <section className='App'>
-      <TwitterFollowCard formatUsername={format} isFollowing={true} userName="Pepdev" name="Pepe Enrrique Giraldo"/>
-      <TwitterFollowCard formatUsername={format} isFollowing={false} userName="Pipdev" name="(otro gemelo malvado)"/>
-      <TwitterFollowCard formatUsername={format} isFollowing userName="juan" name="Pepe Enrrique Giraldo"/>
-      <TwitterFollowCard formatUsername={format}  isFollowing userName="zago" name="(malvado x2)"/>
-    </section>
-    )
+export function App() {
+    const format = (userName) => `@${userName}`;
+    return (
+        <section className='App'>
+            <TwitterFollowCard formatUsername={format} isFollowing={true} userName="Pepdev" name="Santiago Quintero Marulanda" />
+            <TwitterFollowCard formatUsername={format} isFollowing={false} userName="Pipdev" name="un gemelo malvado >:>" />
+            <TwitterFollowCard formatUsername={format} isFollowing={true} userName="juan" name="Santiago Marulanda" />
+            <TwitterFollowCard formatUsername={format} isFollowing={true} userName="zago" name="(malvado x2)" />
+        </section>
+    );
 }
-
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,21 +1,22 @@
-export function TwitterFollowCard ({formatUserName,userName,name,isFollowing }){
-console.log(isFollowing)
+export function TwitterFollowCard ({ formatUsername, userName, name, isFollowing }) {
+    console.log(isFollowing);
     return(
         <article className='tw-followCard'>
-        <header className='tw-followCard-header'>
-            <img className='tw-followCard-avatar'
-            alt="Avatar de persona 1" 
-            src={'https://unavatar.io/github/${username}'}></img>
-            <div className='tw-followCard-info'>
-                <strong>{name}</strong>
-                <span className='tw-followCard-infoUsername'>{formatUserName(userName)}</span>
-            </div>
-        </header>   
-        <aside>
-            <button className='tw-followCard-button'>
-                seguir
-            </button>
-        </aside>
-       </article>
-    )
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar'
+                    alt="Avatar de persona 1" 
+                    src={`https://unavatar.io/github/${userName}`} />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUsername'>{formatUsername(userName)}</span>
+                </div>
+            </header>   
+            <aside>
+                <button className='tw-followCard-button'>
+                    seguir
+                </button>
+            </aside>
+        </article>
+    );
 }
+
